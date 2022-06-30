5fdc24de931effbbcd7ac08c32b97d87d6756b9a
md_mumtaj_alam <mmumtajalam987@gmail.com>
2022-07-01 01:16:33 +0530

    up

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,24 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "../styles/App.css";
+import Home from "./Home";
+import About from "./About";
+import Navigate from "./Navigate";
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="main">
+        <BrowserRouter>
+          <Navigate />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
-
 
 export default App;
---
+++ src/components/Navigate.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navigate = () => {
+  return (
+    <>
+      <a href="/home">Home</a>
+      <a href="/about">About</a>
+    </>
+  );
+};
+
+export default Navigate;
