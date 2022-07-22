b898490fa52b765d4189a218769de387f2513c0d
md_mumtaj_alam <mmumtajalam987@gmail.com>
2022-07-22 23:37:43 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,23 @@
 import React, { Component, useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "../styles/App.css";
 import Home from "./Home";
 import About from "./About";
 import Navigate from "./Navigate";
+import NotFound from "./NotFound";
 
-class App extends Component {
-  render() {
-    return (
-      <div id="main">
-        <BrowserRouter>
-          <Navigate />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    );
-  }
-}
-
+const App = () => {
+  return (
+    <div id="main">
+      <BrowserRouter>
+        <Navigate />
+        <Switch>
+          <Route path="/about" component={About} />
+          <Route path="/" component={Home} />
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+};
 export default App;
--- src/components/Navigate.js
+++ src/components/Navigate.js
@@ -3,7 +3,7 @@ import React from "react";
 const Navigate = () => {
   return (
     <>
-      <a href="/home">Home</a>
+      <a href="/">Home</a>
       <a href="/about">About</a>
     </>
   );
